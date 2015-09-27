7cf86185dba4bb5ac3a9bc8501328ce89545d736
Vincent Gong <gongv93@gmail.com>
2015-09-27 16:14:51 -0400

    Remove change.cpp added to slot.cpp

--- slots.cpp
+++ slots.cpp
@@ -45,6 +45,41 @@ int MainWindow::load()
 	return 1;
 }
 
+void MainWindow::changeThresholdD(double val)
+{
+    changeThresholdI((int) val);
+}
+
+// Slot to process change in threshold value caused by moving the slider.
+
+void MainWindow::changeThresholdI(int val)
+{
+
+    m_filterSlider [0]->blockSignals(true);
+    m_filterSlider [0]->setValue    (val);
+    m_filterSlider [0]->blockSignals(false);
+    m_filterSpinbox[0]->blockSignals(true);
+    m_filterSpinbox[0]->setValue    (val);
+    m_filterSpinbox[0]->blockSignals(false);
+
+
+    // apply new values to stored image
+    preview();
+}
+
+
+
+
+// Slot to reset threshold parameter
+void MainWindow::reset()
+{
+    m_filterSlider[0]->setValue(128);
+
+    // apply new values to stored image
+    preview();
+}
+
+
 void MainWindow::quit()
 {
     // close window
